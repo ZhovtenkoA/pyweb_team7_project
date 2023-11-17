c47a96a8cec113b7bb146b848901d49366d8c025
Oleksandr <115171014+OleksandrPiatnytsia@users.noreply.github.com>
2023-11-18 00:15:28 +0200

    final

--- tests/test_e2e_routes_auth.py
+++ tests/test_e2e_routes_auth.py
@@ -42,36 +42,36 @@ def test_login_user_not_confirmed(client, user):
     assert response.status_code == 401, response.text
     data = response.json()
     assert data["detail"] == "Email not confirmed"
-#
-#
-# def test_login_user(client, session, user):
-#     current_user: User = session.query(User).filter(User.email == user.get('email')).first()
-#     current_user.confirmed = True
-#     session.commit()
-#     response = client.post(
-#         "/auth/login",
-#         data={"username": user.get('email'), "password": user.get('password')},
-#     )
-#     assert response.status_code == 200, response.text
-#     data = response.json()
-#     assert data["token_type"] == "bearer"
-#
-#
-# def test_login_user_wrong_password(client, user):
-#     response = client.post(
-#         "/auth/login",
-#         data={"username": user.get('email'), "password": "password"},
-#     )
-#     assert response.status_code == 401, response.text
-#     data = response.json()
-#     assert data["detail"] == "Invalid password"
-#
-#
-# def test_login_user_wrong_email(client, user):
-#     response = client.post(
-#         "/auth/login",
-#         data={"username": "testtest@gmail.com", "password": user.get('password')},
-#     )
-#     assert response.status_code == 401, response.text
-#     data = response.json()
-#     assert data["detail"] == "Invalid email"
+
+
+def test_login_user(client, session, user):
+    current_user: User = session.query(User).filter(User.email == user.get('email')).first()
+    current_user.confirmed = True
+    session.commit()
+    response = client.post(
+        "/api/auth/login",
+        data={"username": user.get('email'), "password": user.get('password')},
+    )
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["token_type"] == "bearer"
+
+
+def test_login_user_wrong_password(client, user):
+    response = client.post(
+        "/api/auth/login",
+        data={"username": user.get('email'), "password": "password"},
+    )
+    assert response.status_code == 401, response.text
+    data = response.json()
+    assert data["detail"] == "Invalid password"
+
+
+def test_login_user_wrong_email(client, user):
+    response = client.post(
+        "/api/auth/login",
+        data={"username": "testtest@gmail.com", "password": user.get('password')},
+    )
+    assert response.status_code == 401, response.text
+    data = response.json()
+    assert data["detail"] == "Invalid email"
